46bd47b5b7e3d486b2719d6a900276908ba3d0c4
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2021-10-05 09:37:10 -0500

    Activity 4

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,16 @@
+//Welcome Message
+var user = 'Mike';
+var salutaion = 'Hello, ';
+var greeting = salutaion + user + '! Here are the latest Atom reviews.';
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+var price = 20,
+    studentDiscount = .10,
+    sutdentPrice = price - (price * studentDiscount),
+    priceEl = document.getElementById('price'),
+    sutdentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+sutdentPriceEl.textContent = studentPrice.toFixed(2);
